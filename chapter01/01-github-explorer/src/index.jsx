f14fac676c2e56cfc74a400fb220bef154fd4d49
Juniork Oliveira <juniorkoliveira@hotmail.com>
2021-03-11 12:36:14 -0300

    structuring the react

--- chapter01/01-github-explorer/src/index.jsx
+++ chapter01/01-github-explorer/src/index.jsx
@@ -1,5 +1,4 @@
-import React from 'react'
+import {render} from 'react-dom';
+import { App } from './App.jsx';
 
-function App() {
-    return <h1>Hello World</h1>
-}
+render(<App />, document.querySelector('#root'));
